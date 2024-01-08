f4d40a73c6ed49629396161ea5b23617663fb491
jojolee2333 <1243765192@qq.com>
2024-01-08 16:46:54 +0800

    添加uview插件

--- App.vue
+++ App.vue
@@ -1,3 +1,5 @@
+<style lang="scss">@import "@/uni_modules/uview-ui/index.scss";
+</style>
 <script>
 	export default {
 		onLaunch: function() {
---
+++ pages/mine/mine.vue
@@ -0,0 +1,20 @@
+<template>
+    <view class="main-box">
+        <u-cell-group>
+            <u-cell icon="setting-fill" title="个人设置"></u-cell>
+            <u-cell icon="integral-fill" title="会员等级" value="新版本"></u-cell>
+        </u-cell-group>
+    </view>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss" scoped>
+    .main-box {
+        padding: 20px;
+    }
+</style>
